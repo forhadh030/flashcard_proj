e295d3d99760f486f8416d210ebe2ac6a0cda2ac
Hussain <s.j.hussain@afs.com>
2024-02-22 15:18:00 -0600

    my first commit

---
+++ src/main/java/com/project/config/MvcInitalize.java
@@ -0,0 +1,26 @@
+package com.project.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MvcInitalize implements WebApplicationInitializer {
+	
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		// Load Spring web application configuration
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(MvcConfig.class);
+		context.setServletContext(servletContext);
+		
+		// Create and register the DispatcherServlet
+		DispatcherServlet servlet = new DispatcherServlet(context);
+		ServletRegistration.Dynamic registration = servletContext.addServlet("home_site", servlet);
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/");
+	}
+}
