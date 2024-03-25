83705c9ba398f5a4552cb16af5c6def6c8394b83
Syed Forhad Hussain <syedf.hussain7@gmail.com>
2024-03-25 15:28:06 -0500

    Delete flashcard_provider directory

--- flashcard_provider/src/main/java/com/project/FlashcardProvider/repository/FlashcardRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.FlashcardProvider.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.FlashcardProvider.entity.Flashcard;
-
-public interface FlashcardRepo extends CrudRepository<Flashcard, Long>{
-
-}
